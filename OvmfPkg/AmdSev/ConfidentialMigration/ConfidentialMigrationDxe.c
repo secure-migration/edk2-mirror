c46cf443cb42a97b725aca68bc010be3bb2275af
Dov Murik <dovmurik@linux.vnet.ibm.com>
2021-08-12 16:38:29 -0400

    OvmfPkg/AmdSev: Build page table for migration handler
    The migration handler builds its own page tables and switches
    to them. The MH pagetables are reserved as runtime memory.
    
    When the hypervisor asks the MH to import/export a page, the HV
    writes the guest physical address of the page in question to the
    mailbox. The MH uses an identity mapping so that it can read/write
    whatever GPA is requested by the HV. The hypervisor only asks the
    MH to import/export encrypted pages. Thus, the C-Bit can be set
    for every page in the identity map.
    
    The MH also needs to read shared pages, such as the mailbox.
    These are mapped at an offset. The offset must be added to
    the physical address before it can be resolved.
    
    Signed-off-by: Tobin Feldman-Fitzthum <tobin@linux.ibm.com>
    Signed-off-by: Dov Murik <dovmurik@linux.vnet.ibm.com>

--- OvmfPkg/AmdSev/ConfidentialMigration/ConfidentialMigrationDxe.c
+++ OvmfPkg/AmdSev/ConfidentialMigration/ConfidentialMigrationDxe.c
@@ -9,6 +9,8 @@
 #include <Library/BaseMemoryLib.h>
 #include <Library/UefiLib.h>
 
+#include "VirtualMemory.h"
+
 //
 // Functions implemented by the migration handler
 //
@@ -36,6 +38,71 @@ typedef volatile struct {
   UINT32       Done;
 } MH_COMMAND_PARAMETERS;
 
+//
+// Offset for non-cbit mapping.
+//
+#define UNENC_VIRT_ADDR_BASE    0xffffff8000000000ULL
+
+STATIC PAGE_TABLE_POOL   *mPageTablePool = NULL;
+PHYSICAL_ADDRESS  mMigrationHandlerPageTables = 0;
+
+/**
+  Allocates and fills in custom page tables for Migration Handler.
+  The MH must be able to write to any encrypted page. Thus, it
+  uses an identity map where the C-bit is set for every page. The
+  HV should never ask the MH to import/export a shared page. The
+  MH must also be able to read some shared pages. The first 1GB
+  of memory is mapped at offset UNENC_VIRT_ADDR_BASE.
+**/
+VOID
+PrepareMigrationHandlerPageTables (
+  VOID
+  )
+{
+  UINTN                            PoolPages;
+  VOID                             *Buffer;
+  VOID                             *Start;
+  PAGE_MAP_AND_DIRECTORY_POINTER   *PageMapLevel4Entry;
+  PAGE_TABLE_1G_ENTRY              *PageDirectory1GEntry;
+  PAGE_TABLE_1G_ENTRY              *Unenc1GEntry;
+  UINT64                           AddressEncMask;
+
+  PoolPages = 1 + 10;
+  Buffer = AllocateAlignedRuntimePages (PoolPages, PAGE_TABLE_POOL_ALIGNMENT);
+  mPageTablePool = Buffer;
+  mPageTablePool->NextPool = mPageTablePool;
+  mPageTablePool->FreePages  = PoolPages - 1;
+  mPageTablePool->Offset = EFI_PAGES_TO_SIZE (1);
+
+  Start = (UINT8 *)mPageTablePool + mPageTablePool->Offset;
+  ZeroMem(Start, mPageTablePool->FreePages * EFI_PAGE_SIZE);
+
+  AddressEncMask = 1ULL << 47;
+
+  PageMapLevel4Entry = Start;
+  PageDirectory1GEntry = (PAGE_TABLE_1G_ENTRY*)((UINT8*)Start + EFI_PAGE_SIZE);
+  Unenc1GEntry = (PAGE_TABLE_1G_ENTRY*)((UINT8*)Start + 2 * EFI_PAGE_SIZE);
+
+  PageMapLevel4Entry = Start;
+  PageMapLevel4Entry += PML4_OFFSET(0x0ULL);
+  PageMapLevel4Entry->Uint64 = (UINT64)PageDirectory1GEntry | AddressEncMask | 0x23;
+
+  PageMapLevel4Entry = Start;
+  PageMapLevel4Entry += PML4_OFFSET(UNENC_VIRT_ADDR_BASE); // should be 511
+  PageMapLevel4Entry->Uint64 = (UINT64)Unenc1GEntry | AddressEncMask | 0x23;
+
+  UINT64 PageAddr = 0;
+  for (int i = 0; i < 512; i++, PageAddr += SIZE_1GB) {
+    PAGE_TABLE_1G_ENTRY *e = PageDirectory1GEntry + i;
+    e->Uint64 = PageAddr | AddressEncMask | 0xe3; // 1GB page
+  }
+
+  UINT64 UnencPageAddr = 0;
+  Unenc1GEntry->Uint64 = UnencPageAddr | 0xe3; // 1GB page unencrypted
+
+  mMigrationHandlerPageTables = (UINT64)Start | AddressEncMask;
+}
+
 
 VOID
 EFIAPI
@@ -48,8 +115,8 @@ MigrationHandlerMain ()
   DebugPrint (DEBUG_INFO,"Migration Handler Started\n");
 
   MailboxStart = PcdGet32 (PcdConfidentialMigrationMailboxBase);
-  Params = (VOID *)MailboxStart;
-  PageVa = (VOID *)(MailboxStart + 0x1000);
+  Params = (VOID *)(MailboxStart + UNENC_VIRT_ADDR_BASE);
+  PageVa = (VOID *)(MailboxStart + UNENC_VIRT_ADDR_BASE + 0x1000);
 
   DisableInterrupts ();
   Params->Go = 0;
@@ -107,6 +174,8 @@ SetupMigrationHandler (
   if (!PcdGetBool(PcdStartConfidentialMigrationHandler)) {
     return 0;
   }
+
+	PrepareMigrationHandlerPageTables ();
 
   //
   // If VM is migration target, wait until hypervisor modifies CPU state
