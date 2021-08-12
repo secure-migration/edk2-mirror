2e91b45a4d47f82d44ca35e23f6676dc3601c117
Tobin Feldman-Fitzthum <tobin@linux.ibm.com>
2021-08-12 15:41:38 -0400

    OvmfPkg/AmdSev: MH support for mailbox protocol
    The migration handler communicates with the hypervisor
    via a shared mailbox page. The MH can perform four functions
    at the behest of the HV: init, save page, restore page, and
    reset.
    
    Signed-off-by: Tobin Feldman-Fitzthum <tobin@linux.ibm.com>

--- OvmfPkg/AmdSev/ConfidentialMigration/ConfidentialMigrationDxe.c
+++ OvmfPkg/AmdSev/ConfidentialMigration/ConfidentialMigrationDxe.c
@@ -6,14 +6,88 @@
 **/
 
 #include <Library/DebugLib.h>
+#include <Library/BaseMemoryLib.h>
 #include <Library/UefiLib.h>
+
+//
+// Functions implemented by the migration handler
+//
+#define MH_FUNC_INIT          0
+#define MH_FUNC_SAVE_PAGE     1
+#define MH_FUNC_RESTORE_PAGE  2
+#define MH_FUNC_RESET         3
+
+//
+// Return codes for MH functions
+//
+#define MH_SUCCESS            0
+#define MH_INVALID_FUNC     (-1)
+#define MH_AUTH_ERR         (-2)
+
+//
+// Mailbox for communication with hypervisor
+//
+typedef volatile struct {
+  UINT64       Nr;
+  UINT64       Gpa;
+  UINT32       DoPrefetch;
+  UINT32       Ret;
+  UINT32       Go;
+  UINT32       Done;
+} MH_COMMAND_PARAMETERS;
+
 
 VOID
 EFIAPI
 MigrationHandlerMain ()
 {
+  UINT64                       MailboxStart;
+  MH_COMMAND_PARAMETERS        *Params;
+  VOID                         *PageVa;
+
   DebugPrint (DEBUG_INFO,"Migration Handler Started\n");
 
+  MailboxStart = PcdGet32 (PcdConfidentialMigrationMailboxBase);
+  Params = (VOID *)MailboxStart;
+  PageVa = (VOID *)(MailboxStart + 0x1000);
+
+  DisableInterrupts ();
+  Params->Go = 0;
+
+  while (1) {
+    while (!Params->Go) {
+      CpuPause ();
+    }
+    Params->Done = 0;
+
+    switch (Params->Nr) {
+    case MH_FUNC_INIT:
+      Params->Ret = MH_SUCCESS;
+      break;
+
+    case MH_FUNC_SAVE_PAGE:
+      CopyMem (PageVa, (VOID *)Params->Gpa, 4096);
+      Params->Ret = MH_SUCCESS;
+      break;
+
+    case MH_FUNC_RESTORE_PAGE:
+      CopyMem ((VOID *)Params->Gpa, PageVa, 4096);
+      Params->Ret = MH_SUCCESS;
+      break;
+
+    case MH_FUNC_RESET:
+      Params->Ret = MH_SUCCESS;
+      break;
+
+    default:
+      Params->Ret = MH_INVALID_FUNC;
+      break;
+    }
+
+    Params->Go = 0;
+    Params->Done = 1;
+
+  }
 }
 
 /**
